d04c0ff1a25d02830474b4d63b66f64c29bab4a1
Nils-Olov Olsson <nolle_olsson_@hotmail.com>
2022-03-12 16:19:40 +0100

    added login and logout

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
-const express = require("express")
+// npm install passport express-session mongoose express ejs passport-local passport-local-mongoose
+const express = require("express");
+const mongoose = require("mongoose")
+const passport = require("passport")
+const session = require("express-session")
+
+const { User } = require("./models/user")
 
 const app = express()
-const PORT = 3000
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: "avsd1234",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.authenticate("session"))
+
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    if (req.user) {
+        res.render("index.ejs", {username: req.user.username})
+    } else {
+        res.redirect("/login")
+    }
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
+app.get("/logout", (req, res) => {
+    req.logout()
+    res.redirect("/")
 })
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs")
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/"
+}))
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs")
+})
+
+app.post("/signup", async (req, res) => {
+    const {username, password} = req.body
+    const user = new User({username})
+    await user.setPassword(password)
+    await user.save()
+    // console.log(req.body)
+    res.redirect("/login")
+})
+
+mongoose.connect("mongodb://localhost/slutuppgift")
+
+app.listen(PORT, () => {
+  console.log(`Started Express server on port ${PORT}`);
+});
